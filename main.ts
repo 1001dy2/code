6cf7596e99d8ba2bc4a6ca44422fff45035768e1
1001dy2 <115143514+1001dy2@users.noreply.github.com>
2023-03-23 16:45:41 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
-basic.forever(function () {
-	
+radio.onReceivedNumber(function (receivedNumber) {
+    密碼 = receivedNumber
+    basic.showString("" + (密碼))
 })
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(設定)
+})
+let 密碼 = 0
+let 設定 = 0
+radio.setGroup(1)
+設定 = 666
